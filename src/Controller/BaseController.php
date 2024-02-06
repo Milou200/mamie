591cd3f8a4e7b84f617d5b11ee8b049aa02b0aa3
Noa <guillemantnoa29@gmail.com>
2024-02-06 16:07:19 +0100

    mamie1

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,14 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\AjoutCafeType;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Cafe;
+use Doctrine\ORM\EntityManagerInterface;
 
 class BaseController extends AbstractController
 {
-    #[Route('/base', name: 'app_base')]
-    public function index(): Response
+    #[Route('/', name: 'app_acceuil')]
+    public function index(Request $request): Response
     {
         return $this->render('base/index.html.twig', [
             
         ]);
     }
+    #[Route('/noscafes', name: 'app_noscafes')]
+public function cafes(Request $request, EntityManagerInterface $em): Response
+{
+    $cafe = new Cafe();
+    $form = $this->createForm(AjoutCafeType::class,$cafe);
+
+    if($request->isMethod('POST')){
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&&$form->isValid()){
+            $em->persist($cafe);
+            $em->flush();
+            $this->addFlash('notice','Message envoyé');
+            return $this->redirectToRoute('app_noscafes');
+        }
+        }
+return $this->render('base/noscafes.html.twig', [
+    'form' => $form->createView()
+]);
+}
 }
---
+++ src/Entity/Cafe.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CafeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CafeRepository::class)]
+class Cafe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $origine = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->Prix;
+    }
+
+    public function setPrix(string $Prix): static
+    {
+        $this->Prix = $Prix;
+
+        return $this;
+    }
+
+    public function getOrigine(): ?string
+    {
+        return $this->origine;
+    }
+
+    public function setOrigine(string $origine): static
+    {
+        $this->origine = $origine;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
